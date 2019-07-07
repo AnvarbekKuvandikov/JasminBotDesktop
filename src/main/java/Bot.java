a26d01a75141b6eb07e5407b0f922de73bebb349
Loving <loving.uz0000@gmail.com>
2019-07-07 18:46:26 +0500

    Initial commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,73 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.ForwardMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class Bot extends TelegramLongPollingBot {
+
+    Message message;
+//    Scanner scanner=new Scanner(System.in);
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasMessage()) {
+            message=update.getMessage();
+            sendMsg(message);
+        }
+    }
+
+    public synchronized void sendMsg(Message message) {
+//        System.out.println(message.getText());
+//        System.out.println(message.getSticker());
+//        System.out.println(message.getChatId());
+
+        SendMessage sendMessage=new SendMessage();
+        sendMessage.setChatId(message.getChatId());
+        if(message.getText().equals("/start")){
+            sendMessage.setText("Assalomu alaykum "+message.getFrom().getFirstName()+".\nSiz bilan yaqinroq tanishsak bo'ladimi?");
+        }
+        else if(message.getText().equals("/help")){
+            sendMessage.setText("Avtor by:Anvar Quvandiqov");
+        }
+        else {
+            sendMessage.setText("hd");
+        }
+        if(message.getChatId()!=165527805){
+            try {
+                execute(new SendMessage("165527805",
+                        "\nUserName:"+message.getFrom().getUserName()+
+                                "\nFromName:"+message.getFrom().getFirstName()+
+                                " "+message.getFrom().getLastName()+
+                                "ChatId:"+message.getChatId()+
+                                "\nMessageId:"+message.getMessageId()+
+                                "\nText:\n"+message.getText()));
+
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+//        sendMessage.setReplyToMessageId(message.getMessageId());
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public String getBotUsername() {
+        return BotConfig.USER;
+    }
+
+    @Override
+    public String getBotToken() {
+        return BotConfig.TOKEN;
+    }
+}
---
+++ src/main/java/BotConfig.java
@@ -0,0 +1,5 @@
+public class BotConfig {
+    public static final String TOKEN = "831352132:AAHP5MUVZnh0TooqUb3Cwzxg_PrCtRFyDh4";
+    public static final String USER = "Loving_uz_bot";
+
+}
