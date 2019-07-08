9ebc72d64762d2d6bc1a35cd70b72c0c74a2e09e
Loving <loving.uz0000@gmail.com>
2019-07-08 13:00:03 +0500

    Indi ishlar san

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,28 +1,81 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.ForwardMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Bot extends TelegramLongPollingBot {
 
     Message message;
-//    Scanner scanner=new Scanner(System.in);
+    //    Scanner scanner=new Scanner(System.in);
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage()) {
-            message = update.getMessage();
-            sendMsg(message);
+        if(update.hasMessage()){
+            if(update.getMessage().hasText()){
+                if(update.getMessage().getText().equals("Hello")){
+                    try {
+                        execute(sendInlineKeyBoardMessage(update.getMessage().getChatId()));
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else if(update.hasCallbackQuery()){
+
+            String call_data = update.getCallbackQuery().getData();
+            long message_id = update.getCallbackQuery().getMessage().getMessageId();
+            long chat_id = update.getCallbackQuery().getMessage().getChatId();
+
+            EditMessageText new_message = new EditMessageText()
+                    .setChatId(chat_id)
+                    .setMessageId((int)message_id)
+                    .setText("Пример2");
+
+            try {
+                execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
         }
+    }
+
+    public static SendMessage sendInlineKeyBoardMessage(long chatId) {
+        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+        inlineKeyboardButton1.setText("Тык");
+        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
+        inlineKeyboardButton2.setText("Тык2");
+        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+        List<InlineKeyboardButton> keyboardButtonsRow1 = new ArrayList<InlineKeyboardButton>();
+        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<InlineKeyboardButton>();
+        keyboardButtonsRow1.add(inlineKeyboardButton1);
+        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").setCallbackData("CallFi4a"));
+        keyboardButtonsRow2.add(inlineKeyboardButton2);
+        List<List<InlineKeyboardButton>> rowList = new ArrayList<List<InlineKeyboardButton>>();
+        rowList.add(keyboardButtonsRow1);
+        rowList.add(keyboardButtonsRow2);
+        inlineKeyboardMarkup.setKeyboard(rowList);
+        return new SendMessage().setChatId(chatId).setText("Пример").setReplyMarkup(inlineKeyboardMarkup);
     }
 
     public synchronized void sendMsg(Message message) {
 //        System.out.println(message.getText());
 //        System.out.println(message.getSticker());
 //        System.out.println(message.getChatId());
-
         SendMessage sendMessage=new SendMessage();
         sendMessage.setChatId(message.getChatId());
         if(message.getText().equals("/start")){
@@ -54,9 +107,39 @@ public class Bot extends TelegramLongPollingBot {
 //        sendMessage.setReplyToMessageId(message.getMessageId());
         try {
             execute(sendMessage);
+            execute(sendInlineKeyBoardMessage(message.getChatId()));
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
+    }
+
+
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Create a keyboard
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Create a list of keyboard rows
+        List<KeyboardRow> keyboard = new ArrayList<KeyboardRow>();
+
+        // First keyboard row
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Add buttons to the first keyboard row
+        keyboardFirstRow.add(new KeyboardButton("Hi"));
+
+        // Second keyboard row
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Add the buttons to the second keyboard row
+        keyboardSecondRow.add(new KeyboardButton("Help"));
+
+        // Add all of the keyboard rows to the list
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // and assign this list to our keyboard
+        replyKeyboardMarkup.setKeyboard(keyboard);
     }
 
 
